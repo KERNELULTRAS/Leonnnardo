19358c72c7b39b14b5003e5bf7fde881104e3dd7
bedna-KU <kdesi@kdesi.sk>
2014-05-08 18:32:36 +0200

    Send changed

--- worker_uploader.js
+++ worker_uploader.js
@@ -6,8 +6,6 @@ self.onmessage = function (event) {
   xhr.setRequestHeader ("X-File-Name", event.data["name"]); // Custom header - file name
   xhr.setRequestHeader ("X-File-Size", event.data["size"]); // Custom header - file size
   xhr.setRequestHeader ("X-Index", event.data["index"]); // Custom header - file index
-  // var bytes = new Uint16Array(event.data["content"]);
-  // xhr.send (bytes.buffer); // Send headers and content
   xhr.send (event.data["content"]); // Send headers and content
   self.postMessage(xhr.responseText); // Return answer
 };

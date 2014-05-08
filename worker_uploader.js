93642896e8f18161c5799255832d77aea4f4d133
bedna-KU <kdesi@kdesi.sk>
2014-05-08 18:30:10 +0200

    Send changed

--- worker_uploader.js
+++ worker_uploader.js
@@ -2,11 +2,12 @@
 self.onmessage = function (event) {
   var xhr = new XMLHttpRequest(); // New XHR2
   xhr.open ("post", "upload.php", false); // Sync open file
+  xhr.setRequestHeader ("Content-Type", "application/octet-stream");
   xhr.setRequestHeader ("X-File-Name", event.data["name"]); // Custom header - file name
   xhr.setRequestHeader ("X-File-Size", event.data["size"]); // Custom header - file size
   xhr.setRequestHeader ("X-Index", event.data["index"]); // Custom header - file index
-  var bytes = new Uint8Array(event.data["content"]);
-  // xhr.send (event.data["content"]); // Send headers and content
-  xhr.send (bytes.buffer); // Send headers and content
+  // var bytes = new Uint16Array(event.data["content"]);
+  // xhr.send (bytes.buffer); // Send headers and content
+  xhr.send (event.data["content"]); // Send headers and content
   self.postMessage(xhr.responseText); // Return answer
 };

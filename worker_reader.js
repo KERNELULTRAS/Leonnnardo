0844551dd16447164a37de1576407be06a68267a
bedna-KU <kdesi@kdesi.sk>
2014-05-08 18:27:54 +0200

    combed

--- worker_reader.js
+++ worker_reader.js
@@ -1,6 +1,5 @@
 // Worker to read data from chunk
-onmessage = function(event)
-{
-  var reader = new FileReaderSync(); // Define reader 
+onmessage = function(event) {
+  var reader = new FileReaderSync(); // Define reader
   postMessage(reader.readAsArrayBuffer(event.data)); // Return data to main script
 }

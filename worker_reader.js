ef07f1e5587594b7a09726c821612bd20c2e135a
Mario Chorvath <kdesi@kdesi.sk>
2014-07-07 22:06:07 +0200

    Remove file

--- worker_reader.js
+++
@@ -1,5 +0,0 @@
-// Worker to read data from chunk
-onmessage = function(event) {
-  var reader = new FileReaderSync(); // Define reader
-  postMessage(reader.readAsArrayBuffer(event.data)); // Return data to main script
-}

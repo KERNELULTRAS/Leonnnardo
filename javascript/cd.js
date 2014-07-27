69fe38fd7a2d59d5245e9b37bf3d7ca1bf7a69e9
Mario Chorvath <kdesi@kdesi.sk>
2014-07-27 11:18:24 +0200

    Add cd, mkdir, rmdir

---
+++ javascript/cd.js
@@ -0,0 +1,14 @@
+//#####################################################
+// cd.js - change directry on server
+//
+// Part of projekt SLUT https://github.com/bedna-KU/Slut
+// Author: Mario Chorvath - Bedna
+//
+// Licence GNU General Public License
+// Version 3
+// http://www.gnu.org/copyleft/gpl.html
+//#####################################################
+
+function cd () {
+
+}
---
+++ javascript/mkdir_on_server.js
@@ -0,0 +1,34 @@
+//#####################################################
+// mkdir_on_server.js - create directory on server side
+//
+// Part of projekt SLUT https://github.com/bedna-KU/Slut
+// Author: Mario Chorvath - Bedna
+//
+// Licence GNU General Public License
+// Version 3
+// http://www.gnu.org/copyleft/gpl.html
+//#####################################################
+function mkdir_on_server (dir_name) {
+  // console.log ($dir_name);
+  // Encode
+  if (!dir_name) {
+    alert ("You must enter name of directory");
+  }
+  else {
+    dir_name_enc = encodeURIComponent (ab2s (asmCrypto.AES_CBC.encrypt (encodeURIComponent (dir_name), "passwordpassword")));
+    // New XHR2
+    var xhr = new XMLHttpRequest ();
+    xhr.open ("POST", "php/mkdir_on_server.php", false);
+    xhr.setRequestHeader ("X-Dir-Name", dir_name_enc);
+    xhr.send ();
+
+    if (xhr.responseText == "OK") {
+      document.getElementById ("status").innerHTML = "Directory <strong>" + dir_name + " </strong>successfully created";
+    }
+    else {
+      document.getElementById ("status").innerHTML = "Error create directory";
+    }
+    list_files_on_server ();
+    document.getElementById ("dir_name").value = "";
+  }
+}
